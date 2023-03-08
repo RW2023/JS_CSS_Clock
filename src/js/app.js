b82a5e38db719f0ffe3fd490b00ff0719dffe9b6
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 02:59:58 -0500

    create and test "setDat" function

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,6 @@
-function testJs(event) {
-  if (event.key === "t") {
-    const bodyElement = document.querySelector("body");
-    const messageElement = document.createElement("span");
-    messageElement.textContent =
-      "I am connected and working Ryan. You may proceed";
-    bodyElement.appendChild(messageElement);
-  }
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  console.log(seconds);
 }
-
-document.addEventListener("keydown", testJs);
-// I have been deleting this but then i thought it might be helpful and someone else might find it cool
-// like i did. you have to test to see if you are connected right?. This is a function that does that.
 
