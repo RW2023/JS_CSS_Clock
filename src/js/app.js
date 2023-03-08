aef52d1bc9fe077bb9385731131c6d465eb92d40
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 03:30:43 -0500

    add set interval took a minute to debug since I didn't realize i was missing it

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 
-const secondHand = document.querySelector('.secondHand'); //Declare variable outside function to give it global scope
+const secondHand = document.querySelector('.second-hand'); //Declare variable outside function to give it global scope
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds/60) * 360;
-  secondHand.style.transform = `rotate(${seconds}deg)`;
+  const secondsDegrees = ((seconds/60) * 360);
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   console.log(seconds);
 }
 
+setInterval(setDate, 1000);
