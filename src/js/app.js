3561881c1f81da911d692f3c2da5e144112e15f4
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 04:34:31 -0500

    add minute hand transform to function

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
 
 const secondHand = document.querySelector('.second-hand'); //Declare variable outside function to give it global scope
-
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector(".hour-hand");
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds/60) * 360) +90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
+const minsHand = now.getSeconds();
+const minsDegrees = (seconds / 60) * 360 + 90;
+minsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
 }
 
 setInterval(setDate, 1000);
