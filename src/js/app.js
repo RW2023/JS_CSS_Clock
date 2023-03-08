41561694d2a5c41a073465fb5cb4525826b6c7c0
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 05:00:07 -0500

    add logic for other hands to function

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,22 @@
 
+// The following code selects the second hand, minute hand, and hour hand using the class names from the HTML.
 const secondHand = document.querySelector('.second-hand'); //Declare variable outside function to give it global scope
 const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector(".hour-hand");
+const hourHand = document.querySelector('.hour-hand');
+// The setDate function retrieves the current date and time, and calculates the degrees that each hand needs to be rotated based on the current time.
 function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds/60) * 360) +90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-const minsHand = now.getSeconds();
-const minsDegrees = (seconds / 60) * 360 + 90;
-minsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const now = new Date(); // get current date and time
+  const seconds = now.getSeconds(); // get current seconds
+  const secondsDegrees = (seconds / 60) * 360 + 90; // calculate the degree for seconds
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // update seconds hand position
+//Same items are repeated for other  hands on the clock
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 60) * 360 + 90;
+  hourHand.style.transform = `rotates(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
