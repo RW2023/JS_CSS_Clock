3955b0fd0c74d30985415941bdbdfa9a1265f3e1
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 03:19:34 -0500

    add styles to second hand

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds/60) * 360;
+  secondHand.style.transform = `rotate(${seconds}deg)`;
   console.log(seconds);
 }
 
