61fed14bdde23d20787ad98b04d8bb68bfcda634
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-08 03:11:03 -0500

    Declare two variable define secondHand and secondDegree put secondDegree inside function but then was given the choice of how to handle creating secondHand variable. after some research i went with outside the function. The potential unintended consequences are low based on the complexity of this app and the utility is high since the variable may be useful in other points in the code and will be available. Wes put it outside too but gave no reasons as to why prompting the research

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
+
+const secondHand = document.querySelector('.secondHand'); //Declare variable outside function to give it global scope
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
+  const secondsDegrees = (seconds/60) * 360;
   console.log(seconds);
 }
 
